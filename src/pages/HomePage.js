e123119b09219186493441f8ad30c13758fbb9c6
J <2016quezadaj@gmail.com>
2021-11-21 23:25:14 -0500

    Added Container and  Rows to HomePage component

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,11 +1,56 @@
 import React from 'react';
+import Card from 'react-bootstrap/Card';
+import Button from 'react-bootstrap/Button';
+import Container from 'react-bootstrap/Container';
+import Col from 'react-bootstrap/Col';
+import ListGroup from 'react-bootstrap/ListGroup';
+import Row from 'react-bootstrap/Row';
 
 const HomePage = () => {
     return (
         <div>
-           <h1>Home Page</h1>
-           <p>home</p>
-           
+        <Container style={{ width: '40rem' }}>
+        <Row>
+            <Col>
+            1 of 2
+            <Card style={{ width: '18rem' }}>
+  <Card.Img variant="top" src="holder.js/100px180" />
+  <Card.Body>
+    <Card.Title>Card Title</Card.Title>
+    <Card.Text>
+      Some quick example text to build on the card title and make up the bulk of
+      the card's content.
+    </Card.Text>
+    <Button variant="primary">Go somewhere</Button>
+  </Card.Body>
+</Card>
+            </Col>
+            <Col>
+            2 of 2
+            <Card style={{ width: '18rem' }}>
+  <Card.Img variant="top" src="holder.js/100px180" />
+  <Card.Body>
+    <Card.Title>Card Title</Card.Title>
+    <Card.Text>
+      Some quick example text to build on the card title and make up the bulk of
+      the card's content.
+    </Card.Text>
+    <Button variant="primary">Go somewhere</Button>
+  </Card.Body>
+</Card>
+            </Col>
+        </Row>
+        <Row>
+    <Col><h1>Create your Minted Image </h1></Col>
+    <Col>2 of 2: See How</Col>
+  </Row>
+  <Row>
+    <Col>1 of 3</Col>
+    <Col>2 of 3</Col>
+    <Col>3 of 3</Col>
+  </Row>
+</Container>
+            
         </div>
     )
 }
