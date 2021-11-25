86398d5f5086f6491d80793f04884d44606e8327
J <quezada.dev@gmail.com>
2021-11-25 03:15:22 -0500

    Added some more margin to features section and it looks better now

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -50,7 +50,7 @@ const HomePage = () => {
             </Row>
             
             <Row className="gradient--bg">
-              <Col>
+              <Col className="mt-5 mb-5">
               <Row className="mb-5">
               <Col  className="d-flex justify-content-around">
                 <div 
@@ -58,7 +58,7 @@ const HomePage = () => {
                     className="d-flex justify-content-around"
                   > 
                   <div className="align-self-center"> 
-                    <h1>Jot Down Your Thoughts </h1> 
+                    <h1> Express Yourself </h1> 
                   </div>
                   <div> 
                     <Image src="https://imgr.search.brave.com/5sH5KI0ukbsFd2hNklHSMjsBsbnhRX5YcPeNgsnM3k8/fit/736/225/ce/1/aHR0cHM6Ly90c2Uy/Lm1tLmJpbmcubmV0/L3RoP2lkPU9JUC4z/emhHZGhRSTY5REVo/a0picThydTFBSGFF/eCZwaWQ9QXBp" rounded /> 
