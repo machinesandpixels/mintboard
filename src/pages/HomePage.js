446c3d3d3ade067529f8a7bd6be83094674e1725
J <2016quezadaj@gmail.com>
2021-11-24 21:51:27 -0500

    Added new card components to HomePage

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -118,10 +118,10 @@ const HomePage = () => {
             </Row>
 
             <Row xs={1} md={2} className="g-4">
-  {Array.from({ length: 4 }).map((_, idx) => (
+ 
     <Col>
       <Card>
-        <Card.Img variant="top" src="holder.js/100px160" />
+        <Card.Img variant="top" src="/images/whiteboard.jpeg" />
         <Card.Body>
           <Card.Title>Card title</Card.Title>
           <Card.Text>
@@ -131,7 +131,42 @@ const HomePage = () => {
         </Card.Body>
       </Card>
     </Col>
-  ))}
+    <Col>
+      <Card>
+        <Card.Img variant="top" src="/images/whiteboard.jpeg" />
+        <Card.Body>
+          <Card.Title>Card title</Card.Title>
+          <Card.Text>
+            This is a longer card with supporting text below as a natural
+            lead-in to additional content. This content is a little bit longer.
+          </Card.Text>
+        </Card.Body>
+      </Card>
+    </Col>
+    <Col>
+      <Card>
+        <Card.Img variant="top" src="/images/whiteboard.jpeg" />
+        <Card.Body>
+          <Card.Title>Card title</Card.Title>
+          <Card.Text>
+            This is a longer card with supporting text below as a natural
+            lead-in to additional content. This content is a little bit longer.
+          </Card.Text>
+        </Card.Body>
+      </Card>
+    </Col>
+    <Col>
+      <Card>
+        <Card.Img variant="top" src="/images/whiteboard.jpeg" />
+        <Card.Body>
+          <Card.Title>Card title</Card.Title>
+          <Card.Text>
+            This is a longer card with supporting text below as a natural
+            lead-in to additional content. This content is a little bit longer.
+          </Card.Text>
+        </Card.Body>
+      </Card>
+    </Col>
 </Row>
 
 
