4142f66addd06e60af929ff9dcb7b8b406d412b2
J <2016quezadaj@gmail.com>
2021-11-25 02:34:17 -0500

    Refactored BackgroundLayer to Hooks

--- src/components/BackgroundLayer.js
+++ src/components/BackgroundLayer.js
@@ -1,26 +1,25 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { Layer, Image } from "react-konva";
 
 const getMapAsset = () => require(`../assets/whiteboard.png`);
 
-class BackgroundLayer extends React.Component {
-  state = { image: null };
+const BackgroundLayer = ({ mapSize }) => {
 
-  componentDidMount() {
+  const [image, setImage] = useState(null);
+
+  useEffect(() => {
     const image = new window.Image();
-    image.src = getMapAsset();
-    image.onload = () => {
-      this.setState({ image });
-    };
-  }
-
-  render() {
-    return (
-      <Layer>
-        <Image image={this.state.image} height={this.props.mapSize} width={this.props.mapSize} />
-      </Layer>
-    );
-  }
+      image.src = getMapAsset();
+      image.onload = () => {
+        setImage(image);
+      };
+  });
+  
+  return (
+    <Layer>
+      <Image image={image} height={mapSize} width={mapSize} />
+    </Layer>
+  )
 }
 
 export default BackgroundLayer;
