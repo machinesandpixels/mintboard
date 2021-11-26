8e56b41bcb4c17372e6ccd6bdc37e59f360b45af
J <quezada.dev@gmail.com>
2021-11-25 21:25:17 -0500

    Created test build for deployment

---
+++ build/precache-manifest.b27a48a3910bc192291350db08391009.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8cd27c12eee8053c4ac9a6cf33823f8d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "dce86266eab3192799ec",
+    "url": "/static/css/main.0ed026f7.chunk.css"
+  },
+  {
+    "revision": "09885b1fb38087e6dbca",
+    "url": "/static/js/2.4a418792.chunk.js"
+  },
+  {
+    "revision": "9a2af00ad24791f974ff2e28c50faf05",
+    "url": "/static/js/2.4a418792.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dce86266eab3192799ec",
+    "url": "/static/js/main.e4d81279.chunk.js"
+  },
+  {
+    "revision": "7716c43b905e130e4c4a",
+    "url": "/static/js/runtime-main.db9da262.js"
+  },
+  {
+    "revision": "f105b886a949cc0747523651442407f6",
+    "url": "/static/media/whiteboard.f105b886.png"
+  }
+]);
